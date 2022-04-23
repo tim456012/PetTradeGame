ef0ed790385841787a1d450aee417fa4088fbd11
tim456012 <tim168950@gmail.com>
2022-04-23 16:03:01 +0800

    Complete Main Menu Prototype

--- PetTradeGame/Assets/Game/Scripts/Controller/UIController.cs
+++ PetTradeGame/Assets/Game/Scripts/Controller/UIController.cs
@@ -1,3 +1,5 @@
+using System;
+using Game.Scripts.View_Model_Components;
 using TMPro;
 using UnityEngine;
 
@@ -7,19 +9,30 @@ namespace Game.Scripts.Controller
     {
         [SerializeField] private TextMeshProUGUI score;
 
+        [SerializeField] private MainMenuPanel mainMenuPanel;
+        
         private Canvas canvas;
+        
+        public static event EventHandler StartGameEvent;
+        
         //private Tweener transition;
-
+        
         // Start is called before the first frame update
         private void Start()
         {
             canvas = GetComponentInChildren<Canvas>();
 
+            mainMenuPanel.btnStart.onClick.AddListener(OnBtnStartClicked);
         }
 
         public void setScore(int s)
         {
             score.text = $"Score : {s}";
+        }
+
+        private void OnBtnStartClicked()
+        {
+            StartGameEvent?.Invoke(this, EventArgs.Empty);
         }
     }
 }
